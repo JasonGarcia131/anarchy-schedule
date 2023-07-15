d868fa3cd49f1ea9c634d436a98f9b15168ce978
Jason Garcia <jaygarcia0530@gmail.com>
2023-07-15 16:02:27 -0700

    updated base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // const BASE_URL = 'https://anarchyschedule-api.onrender.com';
 
 //Development
-const BASE_URL = 'https://anarchyschedule-api.com';
+const BASE_URL = 'https://anarchyschedule-api.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
